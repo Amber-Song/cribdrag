9ffce7a1722106a92f12a8f1865a555518c4f0b7
Amber-Song <jihongsongnz@gmail.com>
2019-08-19 17:13:49 +1200

    Interactive dragging

--- cribdrag.c
+++ cribdrag.c
@@ -13,7 +13,7 @@ char p[TEXTLENGTH];
 char q[TEXTLENGTH];
 int cipherlen;
 
-void change(int i, char word[]);
+int change(int i, char word[]);
 
 
 
@@ -23,12 +23,12 @@ void change(int i, char word[]);
 */
 void drag(char word[]) {
 
-    int i, j;
+    int i, j, k;
     char ch;
 
     int wordlen = strlen(word);
 
-    for (i = 0; i < cipherlen; i++) {
+    for (i = 0; i < cipherlen - wordlen; i++) {
         for (j = 0; j < wordlen; j++)   {
             ch = ciphertext[i+j] ^ word[j];
             /**
@@ -51,14 +51,19 @@ void drag(char word[]) {
         }
 
         if (j == wordlen)   {
-            change(i, word);
+            k = change(i, word);
+            if (k == 0) {
+                continue;
+            } else  {
+                i = i + j -1;
+            }
         }
     }
 }
 
 
 
-void change(int i, char word[])  {
+int change(int i, char word[])  {
     int j;
     int wordlen = strlen(word);
 
@@ -66,31 +71,36 @@ void change(int i, char word[])  {
         p[i+j] = word[j];
         q[i+j] = ciphertext[i+j] ^ word[j];
     }
-
+    
     for (; j > 0; j--)
     {
         /**
          * if the word doesn't make sense, undo it
         */
         if ( q[i+j-1] <= 65 && q[i+j-1] >= 90 && q[i+j-2] != 32)  {
-            p[i] = '\0';
-            q[i] = '\0';
-            break;
+            for (j = 0; j < wordlen; j++)   {
+                p[i+j] = '\0';
+                q[i+j] = '\0';
+            }
+            return 0;
         }
     }
+    return 1;
 }
 
 
 
-//void getcombineplaintext() {}
-
 int main() {
-    char word[] = "the";
+    char word[50];
     int i;
 
     FILE *fp;
     char filenamecp[] = "cp.dms";
     char filenamecq[] = "cq.dms";
+
+    printf("Input the words you want to drag. \n");
+    gets(word);
+
 
     // Open the file and read into the ciphertext cp.
     fp = fopen(filenamecp, "rb");
@@ -100,7 +110,6 @@ int main() {
     }
     cipherlen = fread(p, 1, TEXTLENGTH, fp);
     fclose(fp);
-    //printf("p: %s\n%lu\n", p, strlen(p)); // test
 
 
     // Open the file and read into the ciphertext cq.
@@ -111,29 +120,23 @@ int main() {
     }
     fread(q, 1, TEXTLENGTH, fp);
     fclose(fp);
-    //printf("q: %s\n,%lu\n", q, strlen(q)); // test
-    //printf("\n\n");
 
 
     // XOR cq and cp, save in the ciphertext
-    
-    //printf("Cipher has length: %d\n", cipherlen);
     for (i = 0; i < cipherlen; i++) {
         ciphertext[i] = p[i] ^ q[i];
-        //printf("%c", ciphertext[i]);
     }
-    //printf("Cipher now has length: %d\n", strlen(ciphertext));
-    //printf("%s\n", ciphertext);
-    //printf("\ncipher: %s\n, %lu\n", ciphertext, strlen(ciphertext)); // test
+
 
     memset(q, 0, TEXTLENGTH);
     memset(p, 0, TEXTLENGTH);
 
 
-    // while(word++ = common_words) {}
     drag(word);
 
-   for (i = 0; i < cipherlen; i++) {
+
+    // print out the plaintext p and q.
+    for (i = 0; i < cipherlen; i++) {
         printf("%c", p[i] ? p[i] : '.');
     }
     printf("\n\n");
